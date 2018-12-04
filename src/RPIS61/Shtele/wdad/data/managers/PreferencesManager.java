526f5f3351adf448d2ccc324f37dc89f52253668
dmitriy <dmitriy.shtele@gmail.com>
2018-12-04 16:19:27 +0400

    test

--- src/RPIS61/Shtele/wdad/data/managers/PreferencesManager.java
+++ src/RPIS61/Shtele/wdad/data/managers/PreferencesManager.java
@@ -1,6 +1,8 @@
 package RPIS61.Shtele.wdad.data.managers;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -12,16 +14,20 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Properties;
+import java.util.Set;
 
 public class PreferencesManager {
     private final static String XML_PATH = "src\\RPIS61\\Shtele\\wdad\\resources\\configuration\\appconfig.xml";
     private Document document;
     private File file;
     private static PreferencesManager instance;
+    private Properties properties;
 
     private PreferencesManager(){
         try{
@@ -29,6 +35,7 @@ public class PreferencesManager {
             DocumentBuilder builder = factory.newDocumentBuilder();
             this.file = new File(XML_PATH);
             this.document = builder.parse(this.file);
+            this.properties = new Properties();
         }
         catch ( IOException | SAXException | ParserConfigurationException e){
             e.printStackTrace();
@@ -42,50 +49,101 @@ public class PreferencesManager {
         return instance;
     }
 
+    public void setProperty(String key, String value){
+        document.getElementsByTagName(key).item(0).setTextContent(value);
+    }
+
+    public String getProperty(String key){
+        return properties.getProperty(key);
+    }
+
+    public void setProperties(Properties properties){
+        for (String p: properties.stringPropertyNames()
+                ) {
+            setProperty(p, properties.getProperty(p));
+        }
+    }
+
+    public Properties getProperties(){
+        return this.properties;
+    }
+
+    public void addBindedObject(String name, String className){
+        Element element = document.createElement("bindedobject");
+        element.setAttribute("name",name);
+        element.setAttribute("class", className);
+        document.getElementsByTagName("server").item(0).appendChild(element);
+        saveXML();
+    }
+
+    public void removeBindedObject(String name){
+        NodeList bindedObjects = document.getElementsByTagName("bindedobject");
+        Element element;
+        for(int i = 0; i < bindedObjects.getLength(); i++){
+            element = (Element) bindedObjects.item(i);
+            if(element.getAttribute("name").equals(name)){
+                element.getParentNode().removeChild(element);
+            }
+        }
+        saveXML();
+    }
+
+    @Deprecated
     public String getCreateRegistry(){
         return document.getElementsByTagName("createregistry").item(0).getTextContent();
     }
 
+    @Deprecated
     public void setCreateRegistry(String param){
         document.getElementsByTagName("createregistry").item(0).setTextContent(param);
     }
 
+    @Deprecated
     public String getRegistryAddress(){
         return document.getElementsByTagName("registryaddress").item(0).getTextContent();
     }
 
+    @Deprecated
     public void setRegistryAddress(String param){
         document.getElementsByTagName("registryaddress").item(0).setTextContent(param);
     }
 
+    @Deprecated
     public String getRegistryPort(){
         return document.getElementsByTagName("registryport").item(0).getTextContent();
     }
 
+    @Deprecated
     public void setRegistryPort(String param){
         document.getElementsByTagName("registryport").item(0).setTextContent(param);
     }
 
+    @Deprecated
     public String getPolicyPath(){
         return document.getElementsByTagName("policypath").item(0).getTextContent();
     }
 
+    @Deprecated
     public void setPolicyPath(String param){
         document.getElementsByTagName("policypath").item(0).setTextContent(param);
     }
 
+    @Deprecated
     public String getUseCodeBaseOnly(){
         return document.getElementsByTagName("usecodebaseonly").item(0).getTextContent();
     }
 
+    @Deprecated
     public void setUseCodeBaseOnly(String param){
         document.getElementsByTagName("usecodebaseonly").item(0).setTextContent(param);
     }
 
+    @Deprecated
     public String getClassProvider(){
         return document.getElementsByTagName("classprovider").item(0).getTextContent();
     }
 
+    @Deprecated
     public void setClassProvider(String param){
         document.getElementsByTagName("classprovider").item(0).setTextContent(param);
     }
--- src/RPIS61/Shtele/wdad/learnxml/XmlTask.java
+++ src/RPIS61/Shtele/wdad/learnxml/XmlTask.java
@@ -20,135 +20,133 @@ public class XmlTask {
     private Document document;
     private File file;
 
-   public XmlTask(String filename){
-       try{
-           DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-           factory.setIgnoringElementContentWhitespace(true);
-           DocumentBuilder builder = factory.newDocumentBuilder();
-           this.file = new File(XML_CATALOG + filename);
-           this.document = builder.parse(this.file);
-           totalCostVerification();
-       } catch (ParserConfigurationException | SAXException | IOException e) {
-           e.printStackTrace();
-       }
-   }
+    public XmlTask(String filename){
+        try{
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setIgnoringElementContentWhitespace(true);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            this.file = new File(XML_CATALOG + filename);
+            this.document = builder.parse(this.file);
+            totalCostVerification();
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-   public XmlTask(File file){
-       try{
-           DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-           DocumentBuilder builder = factory.newDocumentBuilder();
-           this.file = file;
-           this.document = builder.parse(file);
-           totalCostVerification();
-       } catch (ParserConfigurationException | SAXException | IOException e) {
-           e.printStackTrace();
-       }
-   }
+    public XmlTask(File file){
+        try{
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            this.file = file;
+            this.document = builder.parse(file);
+            totalCostVerification();
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-   private void totalCostVerification(){
-       NodeList orders = document.getElementsByTagName("order");
-       Element order, totalCost;
-       NodeList totalCosts;
-       int cost;
-       for(int i = 0; i < orders.getLength(); i++){
-           order = (Element)orders.item(i);
-           totalCosts = order.getElementsByTagName("totalcost");
-           //todo order.getElementsByTagName("totalcost") - в переменную сохраняй результат
-           //todo - разные действия - создаешь тэг если его нет, если есть но с не правильным контентом - изменяешь контент
-           if(totalCosts.getLength() == 0){
-               totalCost = document.createElement("totalcost");
-               totalCost.setTextContent(String.valueOf(calculatingTotalCost(order)));
-               order.appendChild(totalCost);
-               saveXML();
-           }
-           else if(totalCosts.item(0).getTextContent() == null){
-               totalCost = (Element) order.getElementsByTagName("totalcost").item(0);
-               totalCost.setTextContent(String.valueOf(calculatingTotalCost(order)));
-               saveXML();
-           }
-           else{
-               cost = calculatingTotalCost(order);
-               if(Integer.parseInt(totalCosts.item(0).getTextContent()) != cost){
-                   totalCost = (Element) order.getElementsByTagName("totalcost").item(0);
-                   totalCost.setTextContent(String.valueOf(cost));
-                   saveXML();
-               }
-           }
-       }
-   }
+    private void totalCostVerification(){
+        NodeList orders = document.getElementsByTagName("order");
+        Element order, totalCost;
+        NodeList totalCosts;
+        int cost;
+        for(int i = 0; i < orders.getLength(); i++){
+            order = (Element)orders.item(i);
+            totalCosts = order.getElementsByTagName("totalcost");
+            if(totalCosts.getLength() == 0){
+                totalCost = document.createElement("totalcost");
+                totalCost.setTextContent(String.valueOf(calculatingTotalCost(order)));
+                order.appendChild(totalCost);
+                saveXML();
+            }
+            else if(totalCosts.item(0).getTextContent() == null){
+                totalCost = (Element) order.getElementsByTagName("totalcost").item(0);
+                totalCost.setTextContent(String.valueOf(calculatingTotalCost(order)));
+                saveXML();
+            }
+            else{
+                cost = calculatingTotalCost(order);
+                if(Integer.parseInt(totalCosts.item(0).getTextContent()) != cost){
+                    totalCost = (Element) order.getElementsByTagName("totalcost").item(0);
+                    totalCost.setTextContent(String.valueOf(cost));
+                    saveXML();
+                }
+            }
+        }
+    }
 
-   private int calculatingTotalCost(Element order){
-       int sum = 0, cost = 0;
-       NodeList items = order.getElementsByTagName("item");
-       Element item;
-       for(int j = 0; j < items.getLength(); j++){
-           item = (Element)items.item(j);
-           cost = Integer.parseInt(item.getAttribute("cost"));
-           sum += cost;
-       }
-       return sum;
-   }
+    private int calculatingTotalCost(Element order){
+        int sum = 0, cost = 0;
+        NodeList items = order.getElementsByTagName("item");
+        Element item;
+        for(int j = 0; j < items.getLength(); j++){
+            item = (Element)items.item(j);
+            cost = Integer.parseInt(item.getAttribute("cost"));
+            sum += cost;
+        }
+        return sum;
+    }
 
-   public int earningTotal(String officiantSecondName, Calendar calendar){
-       int sum = 0;
-       Element day = findDay(calendar), officiant, order;
-       if(day == null)
-           return -1;
-       NodeList officiants = day.getElementsByTagName("officiant");
-       for(int i = 0; i < officiants.getLength(); i++){
-           officiant = (Element) officiants.item(i);
-           if(officiant.getAttribute("secondname").equals(officiantSecondName)){
-               order = (Element)officiant.getParentNode();
-               sum += Integer.parseInt(order.getElementsByTagName("totalcost").item(0).getTextContent());
-           }
-       }
-       return sum;
-   }
+    public int earningTotal(String officiantSecondName, Calendar calendar){
+        int sum = 0;
+        Element day = findDay(calendar), officiant, order;
+        if(day == null)
+            return -1;
+        NodeList officiants = day.getElementsByTagName("officiant");
+        for(int i = 0; i < officiants.getLength(); i++){
+            officiant = (Element) officiants.item(i);
+            if(officiant.getAttribute("secondname").equals(officiantSecondName)){
+                order = (Element)officiant.getParentNode();
+                sum += Integer.parseInt(order.getElementsByTagName("totalcost").item(0).getTextContent());
+            }
+        }
+        return sum;
+    }
 
-   private Element findDay(Calendar calendar){
-       NodeList days = document.getElementsByTagName("date");
-       Element element;
-       for(int i = 0; i < days.getLength(); i++){
-           element = (Element) days.item(i);
-           if(Integer.parseInt(element.getAttribute("day")) == calendar.get(Calendar.DATE) &&
-                   Integer.parseInt(element.getAttribute("month")) == calendar.get(Calendar.MONTH) + 1&&
-                   Integer.parseInt(element.getAttribute("year")) == calendar.get(Calendar.YEAR)){
-               return element;
-           }
-       }
-       return null;
-   }
+    private Element findDay(Calendar calendar){
+        NodeList days = document.getElementsByTagName("date");
+        Element element;
+        for(int i = 0; i < days.getLength(); i++){
+            element = (Element) days.item(i);
+            if(Integer.parseInt(element.getAttribute("day")) == calendar.get(Calendar.DATE) &&
+                    Integer.parseInt(element.getAttribute("month")) == calendar.get(Calendar.MONTH) + 1&&
+                    Integer.parseInt(element.getAttribute("year")) == calendar.get(Calendar.YEAR)){
+                return element;
+            }
+        }
+        return null;
+    }
 
-   public void removeDay(Calendar calendar){
-       Element day = findDay(calendar);
-       if(day == null)
-           return;
-       document.getDocumentElement().removeChild(day);
-       saveXML();
-   }
+    public void removeDay(Calendar calendar){
+        Element day = findDay(calendar);
+        if(day == null)
+            return;
+        document.getDocumentElement().removeChild(day);
+        saveXML();
+    }
 
-   public void changeOfficiantName(String oldFirstName, String oldSecondName,
-                                  String newFirstName, String newSecondName){
-       NodeList officiantList = document.getElementsByTagName("officiant");
-       Element officiant;
-       for(int i = 0; i < officiantList.getLength(); i++){
-           officiant = (Element) officiantList.item(i);
-           if(officiant.getAttribute("firstname").equals(oldFirstName) && officiant.getAttribute("secondname").equals(oldSecondName)){
-               officiant.setAttribute("firstname", newFirstName);
-               officiant.setAttribute("secondname", newSecondName);
-           }
-       }
-       saveXML();
-   }
+    public void changeOfficiantName(String oldFirstName, String oldSecondName,
+                                    String newFirstName, String newSecondName){
+        NodeList officiantList = document.getElementsByTagName("officiant");
+        Element officiant;
+        for(int i = 0; i < officiantList.getLength(); i++){
+            officiant = (Element) officiantList.item(i);
+            if(officiant.getAttribute("firstname").equals(oldFirstName) && officiant.getAttribute("secondname").equals(oldSecondName)){
+                officiant.setAttribute("firstname", newFirstName);
+                officiant.setAttribute("secondname", newSecondName);
+            }
+        }
+        saveXML();
+    }
 
-   private void saveXML(){
-       try {
-           Transformer transformer = TransformerFactory.newInstance().newTransformer();
-           transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-           transformer.transform(new DOMSource(document), new StreamResult(new FileOutputStream(file)));
-       }
-       catch (FileNotFoundException | TransformerException e){
-           e.printStackTrace();
-       }
-   }
+    private void saveXML(){
+        try {
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.transform(new DOMSource(document), new StreamResult(new FileOutputStream(file)));
+        }
+        catch (FileNotFoundException | TransformerException e){
+            e.printStackTrace();
+        }
+    }
 }
